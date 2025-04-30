768b5e4ced52282738e28b5e6c3c99f975aedc7f
Boyce Wan <boycewan@Boyces-MacBook-Pro.local>
2025-04-30 17:21:29 +0100

    Fix ALLOWED_HOSTS name

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python django_render/manage.py collectstatic --no-input
+python ./django_render/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python django_render/manage.py migrate
+python ./django_render/manage.py migrate
