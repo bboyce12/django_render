0e379114fe80bb571c4b8a19439c889549344093
Boyce Wan <boycewan@Boyces-MacBook-Pro.local>
2025-04-30 17:44:40 +0100

    Fix file structure of django_render

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python ./django_render/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python ./django_render/manage.py migrate
+python manage.py migrate
